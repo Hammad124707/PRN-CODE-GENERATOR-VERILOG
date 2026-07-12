ca563032fe7cae741dd45fa76fc0778624acb57a
nervous solider <hammadsharifsharif@gmail.com>
2026-07-12 20:02:51 +0500

    i changed and corrected gpa_ca_top

--- gps_ca_top.v
+++ gps_ca_top.v
@@ -1,47 +1,17 @@
-module gps_ca_top(
+module gps_ca_top(clk, reset, prn_select,prn_code);
+input clk ;
+input reset ;
+input [5:0] prn_select ;
+output prn_code ;
+wire [9:0] g2_from_reg ;
+wire g2_selected ;
+wire g1_selected ;
+g2_lfsr u3 (.g2(g2_from_reg),.clk(clk),.reset(reset));
+prn_selector u1 (.prn_select(prn_select)  , .g2_selected(g2_selected),.g2_state(g2_from_reg));
+g1_lfsr u2 ( .g1_output(g1_selected),.clk(clk),.reset(reset) );
 
-    input clk,
-    input reset,
-    input [5:0] prn_select,
-    output prn_code,
-    output [9:0] g1_state,
-    output [9:0] g2_state
+assign prn_code = g2_selected ^ g1_selected ;
+endmodule 
 
-);
-// Internal Signals
+ 
 
-wire g1_out;
-wire g2_out;
-wire g2_selected;
-
-// Instantiate G1
-g1_lfsr G1(
-
-    .clk(clk),
-    .reset(reset),
-    .g1_out(g1_out),
-    .g1_state(g1_state)
-
-);
-// Instantiate G2
-g2_lfsr G2(
-
-    .clk(clk),
-    .reset(reset),
-    .g2_out(g2_out),
-    .g2_state(g2_state)
-
-);
-// Instantiate PRN Selector
-prn_selector PRN(
-
-    .prn_select(prn_select),
-    .g2_state(g2_state),
-    .g2_selected(g2_selected)
-);
-
-// Generate GPS C/A Chip
-
-assign prn_code = g1_out ^ g2_selected;
-
-endmodule
