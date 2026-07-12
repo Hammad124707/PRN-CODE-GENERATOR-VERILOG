94015036aa4b3d8f6f5e509e49c58b849d807f92
nervous solider <hammadsharifsharif@gmail.com>
2026-07-12 12:03:42 +0500

    My own written g2_lfsr

--- g2_lfsr.v
+++ g2_lfsr.v
@@ -1,47 +1,20 @@
-module g2_lfsr(
+module g2_lfsr(clk,  reset , g2);
+input clk ;
+input reset ;
+output [9:0] g2 ;
+reg [9:0] g2 ;
+wire feedback ;
+assign feedback = g2[9] ^ g2[8]^g2[1] ^ g2[2]^g2[5] ^ g2[7];
 
-    input clk,
-    input reset,
-
-    output wire g2_out,
-    output wire [9:0] g2_state
-
-);
-	// 10-bit Shift Register
-
-	reg [9:0] g2;
-
-	// Feedback Calculation
-	// Polynomial: x^10 + x^9 + x^8 + x^6 + x^3 + x^2 + 1
-
-	wire feedback;
-
-	assign feedback = g2[9] ^
-        	          g2[8] ^
-	                  g2[7] ^
-        	          g2[5] ^
-	                  g2[2] ^
-        	          g2[1];
-
-	// Outputs
-
-	assign g2_out = g2[9];
-
-	assign g2_state = g2;
-
-	// Shift Register
-
-	always @(posedge clk or posedge reset)
-
-		begin
-
-		    if(reset)
-
-		        g2 <= 10'b1111111111;
-
-		    else
-
-		        g2 <= {feedback, g2[9:1]};
-
-	end
+always@(posedge clk , posedge reset)
+begin
+	if(reset)
+		g2 <= 10'b1111111111;
+	else 
+		g2 <= {feedback, g2[8:0]};
+end
 endmodule
+	
+		
+	
+
